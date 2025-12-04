7ad1a97fd1b2d2cc4d5fe37d55d761b913391b4b
Yiannis <ioannis.roussos@hotmail.com>
2025-12-04 13:11:24 +0200

    initial  commit

---
+++ src/pages/Posts.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+const Posts = () => {
+    const { id } = useParams();
+    console.log(id)
+  return (
+    <div>
+      {id}
+    </div>
+  )
+}
+
+export default Posts
