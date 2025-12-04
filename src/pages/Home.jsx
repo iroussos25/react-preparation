c758d95afe06090332c7e3729b0c1b131825b0d1
Yiannis <ioannis.roussos@hotmail.com>
2025-12-04 17:47:56 +0200

    erron on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const Home = () => {
+  const [users, setUsers] = useState([])
+  async function getUsers() {
+    const {data} = await axios.get('https://jsonplaceholer.typicode.com/users/1')
+    setUsers(data)
+  }
+useEffect(()=>{
+ getUsers();
+}, [])
+
   return (
-    <div>
-      Home
+    <div className="container">
+  <div className="row">
+    <div className="user-list">
+   { 
+    users.map((user) =>(
+
+ <div className="user" key={user.id}>
+        <div className="user-card">
+          <div className="user-card__container">
+            <h3>{user.name}</h3>
+            <p>
+              <b>Email:</b> {user.email}
+            </p>
+            <p>
+              <b>Phone:</b> {user.phone}
+            </p>
+            <p>
+              <b>Website:</b>
+              {user.website}
+            </p>
+          </div>
+        </div>
+      </div>
+    ))
+    
+      }
     </div>
+  </div>
+</div>
   )
 }
 
